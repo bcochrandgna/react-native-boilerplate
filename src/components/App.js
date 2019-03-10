52f0c8fcddcf04c2d4fc4b6c1996e5b2fbdc025e
Bobby Cochran <bobby.cochran@dgna.tech>
2019-03-09 22:20:56 -0500

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+// @flow
+
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const App = () => (
+  <SafeAreaView>
+    <Text>Stubbed Text</Text>
+  </SafeAreaView>
+);
+
+export default App;
